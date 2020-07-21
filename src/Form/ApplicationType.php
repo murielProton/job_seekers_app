9750eb106379ea9a98e486517efa25ed87155807
Muriel Proton <muriel.proton@epitech.eu>
2020-07-21 11:12:15 +0200

    ApplicationType.php label en Français && ContactType.php label en français

--- src/Form/ApplicationType.php
+++ src/Form/ApplicationType.php
@@ -5,6 +5,9 @@ namespace App\Form;
 use App\Entity\Application;
 use App\Entity\Contact;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -18,38 +21,49 @@ class ApplicationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('webSiteName', TextType::class,  ['label' => 'nom du site WEB'])
-            ->add('companyName')
-            ->add('jobAdvertisement')
-            ->add('contact',ContactType::class)
+            ->add('webSiteName', TextType::class,  [
+                'label' => "nom du site WEB",
+                'required'   => false
+            ])
+            ->add('companyName', TextType::class,  [
+                'label' => "nom de l'entreprise ou association*",
+                'required'   => true
+            ])
+            ->add('jobAdvertisement', TextareaType::class,  [
+                'label' => "annonce",
+                'required'   => false,
+            ])/*, [
+                'attr' => ['class' => 'tinymce'],
+            ])*/
+
+            ->add('contact', ContactType::class, [
+                'label' => "Contact",
+                'required'   => false
+            ])
             /* [
                 'class' => Contact::class,
                 'choice_label' => 'Contact',
-            ]*/
-            ->add('postingDate')
-            /*->add('postingDate', 'date',array(
-                'widget' => 'choice',
-                'format' => 'dd-MM-yyyy',
-                'pattern' => '{{ day }}-{{ month }}-{{ year }}',
-                'years' => range(Date('Y'), 2010),
-                'label' => 'nom du site WEB',
-                'input' => 'string',
-                'data'  => '01-01-2001'
-            ))*/
-            /*->add('postingDate', DateType::class,array(
-                'widget' => 'choice',
-                'format' => 'dd-MM-yyyy',
-                'pattern' => '{{ day }}-{{ month }}-{{ year }}',
-                'years' => range(Date('Y'), 2010),
-                'label' => 'nom du site WEB',
-                'input' => 'string',
-                'data'  => '01-01-2001'
-            ))*/
-            ->add('folowUpDate')
-            ->add('answer')
-            ->add('unsolicited')
-            ->add('comments')
-            ->add('jobInterview');
+            ])*/
+            ->add('postingDate', DateType::class, [
+                'label' => "date de la première prise de contact*",
+                'widget' => 'single_text',
+                'required'   => true
+            ])
+            ->add('folowUpDate', DateType::class, [
+                'label' => "date de la première relance*",
+                'widget' => 'single_text',
+                'required'   => false
+            ])
+            ->add('unsolicited', CheckboxType::class, [
+                'label' => "candidature spontanée",
+                'required'   => false
+            ])
+            ->add('comments', TextareaType::class,  [
+                'label' => "commentaires",
+                'required'   => false
+            ])/*, [
+                'attr' => ['class' => 'tinymce'],
+            ])*/;
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,6 +4,12 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +18,26 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('forName')
-            ->add('sirName')
-            ->add('telephone')
-            ->add('eMail')
-            ->add('address')
-        ;
+            ->add('forName', TextType::class, [
+                'label' => "prénom",
+                'required'   => false
+            ])
+            ->add('sirName', TextType::class, [
+                'label' => "nom de Famille",
+                'required'   => false
+            ])
+            ->add('telephone', TelType::class, [
+                'label' => "numero de téléphone",
+                'required'   => false
+            ])
+            ->add('eMail', EmailType::class, [
+                'label' => "adresse couriel",
+                'required' => false
+            ])
+            ->add('address', TextareaType::class,  [
+                'label' => "adresse postale",
+                'required'   => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
