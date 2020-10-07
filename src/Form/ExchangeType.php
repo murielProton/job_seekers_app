471933b521cec2093fc856a8a4b71965e9847d24
Muriel Proton <muriel.proton@epitech.eu>
2020-10-07 16:28:17 +0200

    Formulaire des echanges render

--- src/Form/ExchangeType.php
+++ src/Form/ExchangeType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Exchange;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +16,33 @@ class ExchangeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('date')
-            ->add('schedule')
-            ->add('means')
-            ->add('comments')
-        ;
+            ->add('date', DateType::class, [
+                'label' => "Date du rendez- vous",
+                'widget' => 'single_text',
+                'required' => true
+            ])
+            ->add('schedule', TimeType::class, [
+                'input' => 'datetime',
+                'widget' => 'single_text',
+                'label' => "Horaire du rendez- vous",
+                'required' => true
+            ])
+            ->add('means', ChoiceType::class, [
+                'choices' => [
+                    "Téléphone" => "Téléphone",
+                    "Couriel" => "Couriel",
+                    "Médias Sociaux" => "Médias Sociaux",
+                    "Méssagerie Instantanée" =>  "Méssagerie Instantanée",
+                    "Echanges Vidéo" =>  "Echanges Vidéo"
+                ],
+                'label' => "Moyen de communication",
+                'required' => false
+            ])
+            ->add('comments', TextareaType::class, [
+                'label' => "Description de l'échange et commentaires :",
+                'required' => true,
+                'attr' => ['rows' => '2', 'coles' => '111']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
