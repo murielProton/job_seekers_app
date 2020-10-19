303a421adee6749f155c94bb68a28893cb0a06ec
Muriel Proton <muriel.proton@epitech.eu>
2020-10-19 16:48:36 +0200

    Application de la documentation symfony Security jusqu'au paragrphe 3B) Authenticationg your Users

---
+++ src/DataFixtures/UsersDumy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class UsersDumy extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+
+        // $manager->persist($product);
+        $user->setPassword($this->passwordEncoder->encodePassword($user,'the_new_password'));
+        //$manager->flush();
+    }
+}
